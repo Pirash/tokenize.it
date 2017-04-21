b7408ffd86b5bc1413a00e215fdc48f4ec557493
Piotr Tomaszewski <ptrkntxt@gmail.com>
2017-04-21 14:49:17 +0200

    improved usability

--- frontend/src/js/tokenize.js
+++ frontend/src/js/tokenize.js
@@ -1,87 +1,128 @@
-$(document).ready(function(){
-  $('.tokenize button').click(tokenize);
-  $('.tokenize .input')
-    .change(clearTokenizeOutput)
-    .keydown(clearTokenizeOutput)
-    .focus(selectAll);
-  $('.detokenize button').click(detokenize);
-  $('.detokenize .input')
-    .change(clearDetokenizeOutput)
-    .keydown(clearDetokenizeOutput)
-    .focus(selectAll);
-  $('.detokenize .output')
-    .focus(selectAll);
-  $('.tokenize .output')
-    .focus(selectAll);  
+(function init() {
   
-});
+  var baseUrl = "http://tokenize.it";
+  
+  $(document).ready(function(){
+    $('.tokenize button.start').click(openTokenizer);
+    $('.tokenize button.complete').click(closeTokenizer);
+    $('.detokenize button.start').click(openDetokenizer);
+    $('.detokenize button.complete').click(closeDetokenizer);
+    
+    $('.tokenize button.do').click(tokenize);
+    $('.tokenize .input')
+      .change(clearTokenizeOutput)
+      .keydown(clearTokenizeOutput)
+      .focus(selectAll);
+    $('.detokenize button.do').click(detokenize);
+    $('.detokenize .input')
+      .change(clearDetokenizeOutput)
+      .keydown(clearDetokenizeOutput)
+      .focus(selectAll);
+    $('.detokenize .output')
+      .focus(selectAll);
+    $('.tokenize .output')
+      .focus(selectAll);  
+    
+  });
 
-function clearTokenizeOutput(){
-  $('.tokenize .output').val('');
-}
+  function openTokenizer(){
+    $('.tokenize button.start').slideUp();
+    $('.tokenize .info').slideUp();
+    $('.tokenize .hidden').slideDown();
+    $('.tokenize .output').prop('disabled', true);
+  }
 
-function clearDetokenizeOutput(){
-  $('.detokenize .output').val('');
-}
+  function closeTokenizer(){
+    $('.tokenize button.start').slideDown();
+    $('.tokenize .info').slideUp();
+    $('.tokenize .hidden').slideUp();
+  }  
+  
+  function openDetokenizer(){
+    $('.detokenize button.start').slideUp();
+    $('.detokenize .info').slideUp();
+    $('.detokenize .hidden').slideDown();
+    $('.detokenize .output').prop('disabled', true);
+  }
 
-function selectAll(){
-  $(this).select();
-}
+  function closeDetokenizer(){
+    $('.detokenize button.start').slideDown();
+    $('.detokenize .info').slideUp();
+    $('.detokenize .hidden').slideUp();
+  } 
+  
+  function clearTokenizeOutput(){
+    $('.tokenize .output').val('');
+  }
 
-function tokenize(){
-  var input = $('.tokenize .input').val();
-  $.ajax({
-    url     : "/tokenizer",
-    data    : {value : input},
-    method  : 'POST'
-  }).done(tokenizeDone);
-}
+  function clearDetokenizeOutput(){
+    $('.detokenize .output').val('');
+  }
 
-function tokenizeDone(result){
-  animateOut($('.tokenize .input'));
-  animateIn($('.tokenize .output'), result);
-}
+  function selectAll(){
+    $(this).select();
+  }
 
-function detokenize(){
-  var input = $('.detokenize .input').val();
-  $.ajax({
-    url     : "/tokenizer",
-    data    : {token : input},
-    method  : 'POST'
-  }).done(detokenizeDone);
-}
+  function tokenize(){
+    var input = $('.tokenize .input').val();
+    $('.tokenize .input').prop('disabled', true);
+    $.ajax({
+      url     : baseUrl + "/tokenizer",
+      data    : {value : input},
+      method  : 'POST'
+    }).done(tokenizeDone);
+  }
 
-function detokenizeDone(result){
-  animateOut($('.detokenize .input'));
-  animateIn($('.detokenize .output'), result);
-}
+  function tokenizeDone(result){
+    animateOut($('.tokenize .input'));
+    animateIn($('.tokenize .output'), result);
+    $('.tokenize .output').prop('disabled', false);
+  }
 
-function animateIn(element, text){
-	if(!text) return;
-	var DURATION = 2000,
-	    STEPS = 20,
-	    stepDuration = DURATION / STEPS,
-	    timer = setInterval(doStep, stepDuration),
-		step = 0;
-		
-	function doStep(){
-		step++;
-		element.val(text.substring(0, step * Math.ceil(text.length / STEPS)));
-		if(step >= STEPS) clearInterval(timer);
-	};
-}
+  function detokenize(){
+    var input = $('.detokenize .input').val();
+    $('.detokenize .input').prop('disabled', true);
+    $.ajax({
+      url     : baseUrl + "/tokenizer",
+      data    : {token : input},
+      method  : 'POST'
+    }).done(detokenizeDone);
+  }
 
-function animateOut(element, text){
-	if(!text) text = element.val();
-	var DURATION = 2000,
-	    STEPS = 20,
-	    stepDuration = DURATION / STEPS,
-	    timer = setInterval(doStep, stepDuration),
-		step = STEPS;
-		
-	function doStep(){
-		step--;
-		element.val(text.substring(0, step * Math.ceil(text.length / STEPS)));
-		if(step <= 0) clearInterval(timer);
-	};
-}
+  function detokenizeDone(result){
+    animateOut($('.detokenize .input'));
+    animateIn($('.detokenize .output'), result);
+    $('.detokenize .output').prop('disabled', false);
+  }
+
+  function animateIn(element, text){
+    if(!text) return;
+    var DURATION = 2000,
+        STEPS = 20,
+        stepDuration = DURATION / STEPS,
+        timer = setInterval(doStep, stepDuration),
+      step = 0;
+      
+    function doStep(){
+      step++;
+      element.val(text.substring(0, step * Math.ceil(text.length / STEPS)));
+      if(step >= STEPS) clearInterval(timer);
+    };
+  }
+
+  function animateOut(element, text){
+    if(!text) text = element.val();
+    var DURATION = 2000,
+        STEPS = 20,
+        stepDuration = DURATION / STEPS,
+        timer = setInterval(doStep, stepDuration),
+      step = STEPS;
+      
+    function doStep(){
+      step--;
+      element.val(text.substring(0, step * Math.ceil(text.length / STEPS)));
+      if(step <= 0) clearInterval(timer);
+    };
+  }
+
+} )();
